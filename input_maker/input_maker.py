f23bf6586fbde3f4d27888284e80e3609f3794da
rsingh21 <racquelsingh21@yahoo.com>
2020-02-04 16:25:33 -0800

    make data folder if does not exist

--- input_maker/input_maker.py
+++ input_maker/input_maker.py
@@ -26,12 +26,21 @@ data_directory = script_path + data_folder            #directory to store data f
 
 gb_records,record_names,genome_sizes = utils.get_dir_records(gbfile_directory) #get all records from a dir with genbank files
 
+dirName = data_folder
+
+try:
+    # Create target Directory
+    os.mkdir(dirName)
+    print("Directory " , dirName ,  " Created ")
+except FileExistsError:
+    print("Directory " , dirName ,  " already exists")
+
 for gb_record in gb_records:
     out_path = data_directory + gb_record.name.strip()
     utils.genbank_to_fasta(gb_record, out_path + ".fasta")  #create fasta from gb files
     utils.create_gene_info_csv(gb_record, out_path + "_gene_info_processing.csv") #create gene info csv files
 
-        
+
 
 genome_order = utils.get_genome_order(record_names, args.order_file)
 
@@ -42,26 +51,26 @@ sys.stdout.flush()
 with open(data_directory + "data_files.csv", mode="w", newline="") as datafile:
     data_writer = csv.writer(datafile, delimiter=',')
     data_writer.writerow(["genome", "genes", "connections"])
-    
+
     for i in range(len(genome_order) - 1):
-        
+
         g1 = genome_order[i]
         g2 = genome_order[i+1]
-        
+
         query_fasta = data_directory + g1 + ".fasta"
         subject_fasta = data_directory + g2 + ".fasta"
         out_tsv = data_directory + g1 + "_vs_" + g2 + ".tsv"
         out_csv = data_directory + g1 + "_vs_" + g2 + ".csv"
-        
-        utils.run_tblastx(args, query_fasta, subject_fasta, out_tsv) 
+
+        utils.run_tblastx(args, query_fasta, subject_fasta, out_tsv)
         utils.create_connections_csv(out_tsv,out_csv)
-        
+
         data_writer.writerow([genome_sizes[g1], data_folder + g1 + "_gene_info_processing" + ".csv", data_folder + g1 + "_vs_" + g2 + ".csv" ])
     data_writer.writerow([genome_sizes[genome_order[-1]], data_folder + genome_order[-1] + "_gene_info_processing" + ".csv" ])
 
 
 for f in glob.glob(data_directory + "*.fasta"):
-    
+
     x = re.split("\/", f)
     x = x[-1]
     print('%s\tRemoving %s' % (utils.current_time(), x)),
@@ -69,17 +78,14 @@ for f in glob.glob(data_directory + "*.fasta"):
     os.remove(f)
 
 for f in glob.glob(data_directory + "*.tsv"):
-    
+
     x = re.split("\/", f)
     x = x[-1]
     print('%s\tRemoving %s' % (utils.current_time(), x)),
     sys.stdout.flush()
     os.remove(f)
 
-        
-    
+
+
 ##TODO: Colour schemes, single or double strand?
 ##TODO: Create data directory?
-    
-
-         
